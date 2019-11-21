dca61e01426e55b31bc596865dfd6acd8f47e789
Chamisa Edmo <chamisaedmo@yahoo.com>
2019-11-21 09:22:29 -0700

    reformatting for presentation

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
 <?php
 echo '<h1> A List of Near Earth Objects</h1>
- 		<p>Provided by the NASA Asteroids-NeoWs API</p> 
-     <br/>';
+ 		<h3>Provided by the NASA Asteroids-NeoWs API</h3> ';
 $url = curl_init('https://api.nasa.gov/neo/rest/v1/feed?start_date=2019-11-15&end_date=2019-11-21&api_key=f1NN7ANpqHGvZkORKVl5H8jvXxgAGk9jpWMOOhx0');
-/*$ch = curl_init('https://images-api.nasa.gov/search?q');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);*/
 $data = curl_exec($url);
 $code = curl_getinfo($url, CURLINFO_HTTP_CODE);
-
 
       curl_close($url);
 
